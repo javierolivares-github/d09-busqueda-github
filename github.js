318441b84ccf6125ead253ca00368d7e7360926f
Francisco Javier Olivares Diaz <franciscojavier.olivaresd@gmail.com>
2021-11-18 17:54:23 -0300

    Final

---
+++ github.js
@@ -0,0 +1,22 @@
+class Github {
+  constructor() {
+    this.client_id = '9ef2d05a4a813a9780c3 ';
+    this.client_secret = '68f360213fd7301179c279e3c027e035186158bf';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    }
+  }
+}
